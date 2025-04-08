b33b3470ba9ea951c56d2261fb3eb748cde95cf2
spandana-hr <hrspandana07@gmail.com>
2025-04-08 14:15:06 +0530

    Add files via upload

---
+++ assignmentoperator.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class assignmentoperator {
+        public static void main(String[] args){
+
+                int a;
+                a =10;
+                System.out.println("a is: "+a);
+
+
+               //add 2 to a
+                a +=2;
+                System.out.println("Addition is: "+a);
+
+
+                //sub a=-2
+                a -=2;
+                System.out.println("Subtraction is: "+a);
